ddd4dd67bd673dc1b7672075cfdc950cfcf14a0d
Telmo "Trooper <telmo.trooper@gmail.com>
2020-01-26 19:19:57 -0300

    Removed code I won't use

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
-use actix_web::{web, App, HttpResponse, HttpRequest, HttpServer, Responder};
-use actix_web::get;
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello, {}!", &name)
-}
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-//            .route("/{name}", web::get().to(greet))
-            .service(hello)
+            .service(home)
     })
         .bind("127.0.0.1:8000")?
         .run()
         .await
 }
 
-#[get("/hello")]
-async fn hello() -> impl Responder {
+#[get("/")]
+async fn home() -> impl Responder {
     HttpResponse::Ok().body("Hello")
 }
