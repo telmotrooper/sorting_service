0f4a98fb2d31a990433fe66f19982140ce96db7f
Telmo "Trooper <telmo.trooper@gmail.com>
2020-03-14 00:06:55 -0300

    Implemented logger

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_imports)]
+
+extern crate env_logger;
 
 // Top level module declarations
 mod models;
 mod routes;
 
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let address = "127.0.0.1:8000";
-    println!("Running web server on http://{}", address);
-    HttpServer::new(|| App::new().service(routes::home).service(routes::read_book))
-        .bind(address)?
-        .run()
-        .await
+    println!("Running web server on http://{}\n", address);
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(routes::home)
+            .service(routes::read_book)
+    })
+    .bind(address)?
+    .run()
+    .await
 }
