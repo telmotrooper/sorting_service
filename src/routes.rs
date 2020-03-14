f048e4aad3ff3ab8dab29a5cd7f36dc952fe1a68
Telmo "Trooper <telmo.trooper@gmail.com>
2020-03-14 02:02:39 -0300

    Quick refactoring

--- src/routes.rs
+++ src/routes.rs
@@ -16,12 +16,16 @@ pub async fn sort_books(input: Json<models::SortingInput>) -> Result<String> {
         }
     }
 
-    let groups: Vec<&str> = input.order_by.split(",").collect();
+    // e.g. ["title desc", " author asc"]
+    let rules: Vec<&str> = input.order_by.split(",").collect();
 
-    let x: Vec<Vec<&str>> = groups
+    // e.g. [["title", "desc"], ["author", "asc"]]
+    let processed_rules: Vec<Vec<&str>> = rules
         .iter()
-        .map(|group| group.split(" ").filter(|x| *x != "").collect())
+        .map(|rule| rule.split(" ").filter(|text| *text != "").collect())
         .collect();
+
+    // TODO: multi-level sorting
 
     let title = input.books[0].title.as_ref().unwrap();
     let author = input.books[0].author.as_ref().unwrap();
