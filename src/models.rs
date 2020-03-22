a73b5ecc260973681fe978d23f00973e90874ff2
Telmo Silva <tvsilva@tech6.com.br>
2020-03-22 09:58:58 -0300

    Changed model

--- src/models.rs
+++ src/models.rs
@@ -11,6 +11,5 @@ pub struct Book {
     pub title: Option<String>,
     pub subtitle: Option<String>,
     pub author: Option<String>,
-    pub isbn_10: Option<String>,
-    pub isbn_13: Option<String>,
+    pub edition_year: Option<String>,
 }
--- src/utils.rs
+++ src/utils.rs
@@ -5,8 +5,7 @@ pub fn get_from_book<'b>(book: &'b Book, attribute: &str) -> &'b Option<String> 
         "title" => &book.title,
         "subtitle" => &book.subtitle,
         "author" => &book.author,
-        "isbn_10" => &book.isbn_10,
-        "isbn_13" => &book.isbn_13,
+        "edition_year" => &book.edition_year,
         _ => &book.title,
     };
 }
